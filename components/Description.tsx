b2c3059bb192a7bd2a3c386711c297b6ddf4206c
Lukas.J.Han <lukas.j.han@gmail.com>
2024-03-13 23:31:31 +0900

    style: remove blinking in prev-button and improve descriptions

--- components/Description.tsx
+++ components/Description.tsx
@@ -16,15 +16,15 @@ export const Description = ({ step }: { step: string }) => {
         <br />
         <p>
           <span className="font-bold">
-            {isMission1Complete(step) ? "[ ✔️ ]" : "[ ]"} Mission :
+            {isMission1Complete(step) ? '[ ✔️ ]' : '[ ]'} Mission:
           </span>
-          나는 학교를 상대로 재학 증명서를 받아내야 합니다.
+          {' 나는 학교를 상대로 재학 증명서를 받아내야 합니다.'}
         </p>
         <p>
           <span className="font-bold">
-            {isMission2Complete(step) ? "[ ✔️ ]" : "[  ]"} Mission :
+            {isMission2Complete(step) ? '[ ✔️ ]' : '[  ]'} Mission:
           </span>
-          받은 재학 증명서를 과외 플랫폼에 제출해야 합니다.
+          {' 받은 재학 증명서를 과외 플랫폼에 제출해야 합니다.'}
         </p>
       </AlertDescription>
     </Alert>
