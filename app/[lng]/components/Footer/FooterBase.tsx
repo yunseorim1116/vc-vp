44e7c915c3b47906b8ef70f7f70232481b5ee620
tree <hhyjs2r@gmail.com>
2024-03-15 01:56:54 +0900

    refactor: change footer logic

--- app/[lng]/components/Footer/FooterBase.tsx
+++ app/[lng]/components/Footer/FooterBase.tsx
@@ -9,21 +9,23 @@ interface FooterProps {
 }
 
 export const FooterBase = ({ t, lng }: FooterProps) => {
+  const otherLanguages = languages.filter((language) => language !== lng);
+
+  const renderLanguageLinks = () => {
+    return otherLanguages.map((language, index) => (
+      <span key={language}>
+        {index > 0 && " or "}
+        <Link href={`/${language}`}>{language}</Link>
+      </span>
+    ));
+  };
+
   return (
-    <div style={{ marginTop: 50 }}>
+    <div>
       <Trans i18nKey="languageSwitcher" t={t}>
         Switch from <strong>{lng}</strong> to:
       </Trans>
-      {languages
-        .filter((l) => lng !== l)
-        .map((l, index) => {
-          return (
-            <span key={l}>
-              {index > 0 && " or "}
-              <Link href={`/${l}`}>{l}</Link>
-            </span>
-          );
-        })}
+      {renderLanguageLinks()}
     </div>
   );
 };
