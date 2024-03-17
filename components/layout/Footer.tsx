33b96a34bac7128ab91c8644cb49b6bccf2af8ba
tree <hhyjs2r@gmail.com>
2024-03-17 23:36:07 +0900

    refactor: remove unused file content

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { useTranslation } from "../../app/i18n/client";
-import Link from "next/link";
-import { languages } from "@/app/i18n/settings";
 import { TFunction } from "i18next";
 import { SelectLanguage } from "./Select";
-
-export const Footer = ({ lng }: { lng: string }) => {
-  const { t } = useTranslation(lng, "footer");
-  return <FooterBase t={t} lng={lng} />;
-};
 
 interface FooterProps {
   t: TFunction<any, undefined>;
@@ -17,16 +9,5 @@ interface FooterProps {
 }
 
 export const FooterBase = ({ t, lng }: FooterProps) => {
-  const otherLanguages = languages.filter((language) => language !== lng);
-
-  const renderLanguageLinks = () => {
-    return otherLanguages.map((language, index) => (
-      <span key={language}>
-        {index > 0 && " or "}
-        <Link href={`/${language}`}>{language}</Link>
-      </span>
-    ));
-  };
-
   return <SelectLanguage t={t} lng={lng} />;
 };
