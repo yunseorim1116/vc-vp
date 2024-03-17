436f8ebac07f3d71b5f34b1b593121c0b1aaf244
tree <hhyjs2r@gmail.com>
2024-03-17 22:20:15 +0900

    fix: style

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "./globals.css";
+import "../globals.css";
 import ReactQueryProviders from "../hooks/useReactQuery";
 import Header from "@/components/layout/Header";
 import { languages } from "../i18n/settings";
