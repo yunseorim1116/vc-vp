e7096feb43b736e90ed3045aef166f4728e0b70d
Lukas.J.Han <lukas.j.han@gmail.com>
2024-03-18 00:24:07 +0900

    fix: strings and style (#7)

--- components/Holder.tsx
+++ components/Holder.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import "./button.css";
-import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
-import { Chapter } from "./common/Chapter";
-import { Explain } from "./common/Explain";
-import axios from "axios";
-import { Content } from "./common/Content";
-import JsonFormatter from "react-json-formatter";
-import { jsonStyle } from "@/const/style";
-import { ISSUE, VERIFY } from "@/const/status";
-import { Skip } from "./common/Skip";
-import PresentaionCode from "@/components/mdx/presentaion.mdx";
-import { useTranslation } from "@/app/i18n/client";
-import { DialogDemo } from "./common/DialogDemo";
+import './button.css';
+import { Button } from '@/components/ui/button';
+import React, { useState } from 'react';
+import { Chapter } from './common/Chapter';
+import { Explain } from './common/Explain';
+import axios from 'axios';
+import { Content } from './common/Content';
+import JsonFormatter from 'react-json-formatter';
+import { jsonStyle } from '@/const/style';
+import { ISSUE, VERIFY } from '@/const/status';
+import { Skip } from './common/Skip';
+import PresentaionCode from '@/components/mdx/presentaion.mdx';
+import { useTranslation } from '@/app/i18n/client';
+import { DialogDemo } from './common/DialogDemo';
 
 export const Holder = ({
   setStep,
@@ -44,14 +44,14 @@ export const Holder = ({
 
   return (
     <div>
-      <Chapter text={t("me")} text2="(Holder)" desc={t("disclosure")} />
+      <Chapter text={t('me')} text2="(Holder)" desc={t('disclosure')} />
       <br />
-      <Explain description={t("presentDesc1")} />
+      <Explain description={t('presentDesc1')} />
       <Button
         onClick={createPresent}
-        className={`mr-4 mb-8 ${presetation ? "" : "blinking"}`}
+        className={`mr-4 mb-8 ${presetation ? '' : 'blinking'}`}
       >
-        {t("letsIssue")}
+        {presetation ? 'Done!' : t('present')}
       </Button>
 
       <DialogDemo>
@@ -62,15 +62,18 @@ export const Holder = ({
       {presetation ? (
         <>
           <Content>{presetation}</Content>
-          <Explain description={t("presentDesc2")} />
-          <Button onClick={getClaims} className="blinking mb-8">
-            {t("getClaims")}
+          <Explain description={t('presentDesc2')} />
+          <Button
+            onClick={getClaims}
+            className={`${claims ? '' : 'blinking'} mb-8`}
+          >
+            {claims ? 'Done!' : t('getClaims')}
           </Button>
         </>
       ) : (
         <>
           <Content>
-            <p className="text-gray-500 mb-4"> {t("plzIssueCredential")}</p>
+            <p className="text-gray-500 mb-4"> {t('plzIssueCredential')}</p>
           </Content>
           <div className="flex justify-between">
             <Button
@@ -89,7 +92,7 @@ export const Holder = ({
           <Content>
             <JsonFormatter json={claims} tabWith={5} jsonStyle={jsonStyle} />
           </Content>
-          <Explain description={t("presentDesc3")} />
+          <Explain description={t('presentDesc3')} />
           <p className="text-xl font-bold  mb-4">[ ✔️ ] Mission2 Complete !</p>
 
           <div>
